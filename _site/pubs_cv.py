2dfb676245d356c16accff33d0f26136dd4bab89
momchilmm <momchilmm@gmail.com>
2019-09-09 16:14:58 -0700

    Almost done it seems

--- _site/pubs_cv.py
+++
@@ -1,8 +0,0 @@
-# Create a list of publications based on the entries in my CV
-import bibtexparser
-
-with open('/Users/momchilminkov/Drive/Postdoc/Cv/Master/my_bib.bib') as bibtex_file:
-# with open('test.bib') as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
-
-print(bib_database.entries)
